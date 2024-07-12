1ed3df990cb9dfd0753cf544f16c14ca9a9b9992
Ian Grzembski <iangrz2003@gmail.com>
2024-07-12 13:55:03 -0400

    Added searching functionality

--- node_server/server.js
+++ node_server/server.js
@@ -1,19 +1,34 @@
-import express from "express";
+import express, { query } from "express";
 import { promises as fs } from 'fs';
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import RateLimit from 'express-rate-limit';
 
 dotenv.config();
 const mongoUrl = process.env.MONGO_DB_URL;
 const flaskUrl = process.env.ML_SERVER_URL
 const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
+
 
 const client = await MongoClient.connect(mongoUrl);
 const db = client.db(dbName);
+const collection = db.collection(collectionName);
 
 const PORT = 3001;
 const app = express();
+
+// set up rate limiter: maximum of 60 requests per minute
+const maxRequests = 60;
+const numSeconds = 60;
+const limiter = RateLimit({
+  windowMs: numSeconds * 1000, // 1000 milliseconds = 1 second
+  max: maxRequests, 
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
 
 app.use(express.json());
 app.use(cors());
@@ -28,6 +43,19 @@ app.get('/api', async (req, res) => {
         res.status(500).send("Can't connect to ML server");
     }
 });
+
+app.post('/api/search', async (req, res) => {
+    try{
+        const queryParams = req.query;
+        const result = await collection.find(queryParams).toArray()
+        res.json(result)
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Can't search right now.");
+    }
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);

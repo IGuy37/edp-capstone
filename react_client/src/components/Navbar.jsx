62c92db7221676bfdba411292f3479f438805daf
Guillermo <pulparindo1414@gmail.com>
2024-07-15 16:42:03 -0400

    make logo smaller

--- react_client/src/components/Navbar.jsx
+++ react_client/src/components/Navbar.jsx
@@ -12,19 +12,31 @@ function Navbar_1(props) {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="/" >
-          <img src="/src/assets/logo.png"></img>
+      <Navbar.Brand href="/" >
+      <img className = "img-responsive" src="/src/assets/logo.png"></img>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px',maxWidth:'100%'}}
+            style={{ maxHeight: '100px',maxWidth:'100%' }}
             navbarScroll
           >
-            <Nav.Link href="/employee">Search Employees</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/register">Register</Nav.Link>
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
           </Nav>
           <Search setEmployee={props.setEmployee}/>
         </Navbar.Collapse>

0bcba2606890ed2554009a9e69a79d18d4ded8cc
Guillermo <pulparindo1414@gmail.com>
2024-07-15 16:08:44 -0400

    changes added into logo

--- react_client/src/components/Navbar.jsx
+++ react_client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar_1(props) {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px',maxWidth:'100%' }}
+            style={{ maxHeight: '100px',maxWidth:'100%'}}
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>

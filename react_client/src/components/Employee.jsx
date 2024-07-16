4e6f3d3a815fda25335d17afe009cff86d9bfe74
Ian Grzembski <iangrz2003@gmail.com>
2024-07-16 09:39:12 -0400

    Updated tab spacing in Employee.jsx

--- react_client/src/components/Employee.jsx
+++ react_client/src/components/Employee.jsx
@@ -6,33 +6,30 @@ import profile from '../assets/profile-icon.png'
 export default function Employee(props){
     const employee = props.data;
     return (
-        <>
         <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={profile} />
-      <Card.Body>
-      <Card.Title>{employee.name}</Card.Title>
-            <div>Phone Number: {employee.phone_number}</div>
-            <div>Job Role: {employee.job_role}</div>
-            <div>Work Location: {employee.location}</div>                
-            <div>Salary: ${employee.salary}</div>
-            <div>Boss:&nbsp;
-                {
-                    (!employee.boss) ? "None" :
-                    <Subordinate name={employee.boss} setEmployee={props.setEmployee} />
-                }
-            </div>
-            <div>Subordinates:&nbsp; 
-                {
-                    (!employee.subordinates) ? "None" :
-                    employee.subordinates.map(
-                        sub => <Subordinate key={sub} name={sub} setEmployee={props.setEmployee}/>
-                    )
-                }
-            </div>
-            <Prediction job_role = {employee.job_role} location={employee.location}/>
-      </Card.Body>
-    </Card>
-            
-        </>
-    )
+            <Card.Img variant="top" src={profile} />
+            <Card.Body>
+                <Card.Title>{employee.name}</Card.Title>
+                <div>Phone Number: {employee.phone_number}</div>
+                <div>Job Role: {employee.job_role}</div>
+                <div>Work Location: {employee.location}</div>                
+                <div>Salary: ${employee.salary}</div>
+                <div>Boss:&nbsp;
+                    {
+                        (!employee.boss) ? "None" :
+                        <Subordinate name={employee.boss} setEmployee={props.setEmployee} />
+                    }
+                </div>
+                <div>Subordinates:&nbsp; 
+                    {
+                        (!employee.subordinates) ? "None" :
+                        employee.subordinates.map(
+                            sub => <Subordinate key={sub} name={sub} setEmployee={props.setEmployee}/>
+                        )
+                    }
+                </div>
+                <Prediction job_role = {employee.job_role} location={employee.location}/>
+            </Card.Body>
+        </Card>  
+    );
 }

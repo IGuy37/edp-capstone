4cc647e33b7553105950e3887669ba31c8410f71
Guillermo <pulparindo1414@gmail.com>
2024-07-16 10:06:44 -0400

    Card Centered

--- react_client/src/components/Navbar.jsx
+++ react_client/src/components/Navbar.jsx
@@ -14,25 +14,13 @@ export default function Navbar_1(props) {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="nav"
+            className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px',maxWidth:'100%' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Nav.Link href="/employee">Search Employees</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
           </Nav>
           <Search setEmployee={props.setEmployee}/>
         </Navbar.Collapse>
